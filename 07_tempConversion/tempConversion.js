a6b780c1bd0cf3b9f4e8caf65084239a54401e42
NickrodBasiri <basirinickrod@gmail.com>
2024-07-31 00:22:55 -0400

    Finished seventh exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (5 / 9) * (tempF - 32);
+  tempC = Math.round(tempC * 10) / 10;
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (tempC * (9 / 5)) + 32;
+  tempF = Math.round(tempF * 10) / 10;
+  return tempF;
 };
 
 // Do not edit below this line
