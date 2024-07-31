a35fba74c1d78c9021f1f2a7a793e15b7f66c9f8
NickrodBasiri <basirinickrod@gmail.com>
2024-07-30 22:18:30 -0400

    Finished second exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
 
-};
+    if (num < 0) return "ERROR";
+    for (i = 1; i <= num; i++) {
+        result = result + string;
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
