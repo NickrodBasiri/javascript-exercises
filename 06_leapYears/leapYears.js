000587bbdc5f4e97cb5bcec38c751e52fa602a75
NickrodBasiri <basirinickrod@gmail.com>
2024-07-31 00:12:37 -0400

    Finished sixth exercise.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+
+    // if year is divisible by four it is leap year
+    // if year is not divisble by 100 && divisible by 400 it is leap year
+
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            return true;
+        }       
+    } else if (year % 4 == 0 && year % 100 != 0) {
+        return true;
+    } else if (year % 100 == 0) {
+        return false;
+    } else {
+        return false;
+    }
+    return false;
 };
 
 // Do not edit below this line
