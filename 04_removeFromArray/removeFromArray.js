23669bad8f44bde399fbce95a46a416aba66cc3e
NickrodBasiri <basirinickrod@gmail.com>
2024-07-30 23:14:26 -0400

    Finished fifth exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item) {
+    
+    for (i = 0; i <= array.length; i++) {
+        if (array[i] == item) {
+            array = array.slice(i, 1);
+            return array;
+        }
+        return array;
+    }
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let smaller = num1;
+    let larger = num2;
+    
+    if (!Number.isInteger(num1) || num1 < 0) {
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(num2) || num2 < 0) {
+        return "ERROR";
+    }
+
+    if (num1 > num2) {
+        larger = num1;
+        smaller = num2;
+    }
+
+    let sum = 0;
+    for (i = smaller; i <= larger; i++) {
+        sum = sum + i;
+    }
+    return sum;
 
 };
 
