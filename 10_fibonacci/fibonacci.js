56e5009276655f15410e32cf39731421720f0a0f
NickrodBasiri <basirinickrod@gmail.com>
2024-08-03 16:44:30 -0300

    Finished exercise 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const sequence = [1, 1];
+    if (num == 0) {
+        return 0;
+    }
+    if (num < 0) {
+        return "OOPS";
+    }
+    for (i = 1; i <= num - 2; i++) {
+        sequence.push(sequence[i] + sequence[i - 1]);
+    }
+    return sequence[sequence.length - 1];
 };
 
 // Do not edit below this line
