74e3368e1083da506ecdb46196accda5631f19c5
NickrodBasiri <basirinickrod@gmail.com>
2024-08-03 16:30:50 -0300

    Finished exercise 9 (had to use solution).

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const validChar = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+    const goodString = str
+    .toLowerCase()
+    .split("")
+    .filter((character) => validChar.includes(character))
+    .join("");
+
+    const reversedString = goodString.split("").reverse().join("");
+
+    return goodString === reversedString;
 };
 
 // Do not edit below this line
